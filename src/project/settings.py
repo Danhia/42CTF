52c2dcdec3fd007fcbd7ff2b447eeedd9ba57b9b
ix <0x00fi@protonmail.com>
2021-09-06 20:15:03 +0200

    last

--- src/project/settings.py
+++ src/project/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from django.utils.translation import gettext_lazy as _
 from local_settings import *
-try:
-    from local_settings import *
-except ImportError:
-    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
