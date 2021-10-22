86cc6070e138dc80edfb9c23c0e0e7b1480ef973
ix <0x00fi@protonmail.com>
2021-10-22 11:45:50 +0200

    Events done

--- src/events/views.py
+++ src/events/views.py
@@ -27,17 +27,44 @@ def events(request):
 def chall_event_info(request, event_slug, chall_slug):
     event_info  = get_object_or_404(Event, slug=event_slug)
     ctf_info    = get_object_or_404(CTF, event__slug=event_info.slug, slug=chall_slug)
+    eventisover = False
+    alreadyflag = False
+    congrat     = False
+    wrongflag   = False 
+    errorform   = False
+    notsub      = False
     if request.user.is_authenticated and not request.user.is_staff:
         userScore = Scores.objects.filter(event=event_info, user=request.user)
         if not userScore:
             return redirect('/')
+    if request.GET.get('EventIsOver'):
+        eventisover = True
+    if request.GET.get('AlreadyFlagged'):
+        alreadyflag = True
+    if request.GET.get('Congrat'):
+        congrat     = True
+    if request.GET.get('WrongFlag'):
+        wrongflag   = True 
+    if request.GET.get('ErrorInForm'):
+        errorform   = True
+    if request.GET.get('NotRegistered'):
+        notsub      = True
     solved_list = CTF_flags.objects.filter(ctf=ctf_info).order_by('flag_date')
     description = get_description_by_lang(ctf_info)
-    return render(request, 'events/ctf_info.html', { 'ctf' : ctf_info, 'event':event_info, 'solved_list': solved_list, 'description': description})
+    return render(request, 'events/ctf_info.html', { 'ctf' : ctf_info, 'event':event_info, 'solved_list': solved_list, 'description': description, 'eventisover': eventisover, 'alreadyflag': alreadyflag, 'congrat': congrat, 'wrongflag': wrongflag, 'errorform': errorform, 'notsub': notsub})
 
 def event(request, event_slug):
     event_info  = get_object_or_404(Event, slug=event_slug)
     IsRegistered = False
+    wrongpwd = False
+    alreadyregistered = False
+    subisover = False
+    if request.GET.get('WrongPassword'):
+        wrongpwd = True
+    if request.GET.get('AlreadyRegistered'):
+        alreadyregistered = True
+    if request.GET.get('SubscriptionIsOver'):
+        subisover = True
     if request.user.is_authenticated:
         userScore = Scores.objects.filter(event=event_info, user=request.user)
         if userScore:
@@ -46,9 +73,9 @@ def event(request, event_slug):
         if request.user.is_authenticated:
             if request.user.is_staff is False:
                 if not userScore:
-                    return render(request, 'events/event_pwd.html', {'event' : event_info, 'logged': True})
+                    return render(request, 'events/event_pwd.html', {'event' : event_info, 'logged': True, 'wrongpwd': wrongpwd, 'alreadyregistered': alreadyregistered})
         else:
-            return render(request, 'events/event_pwd.html', {'event' : event_info, 'logged': False})
+            return render(request, 'events/event_pwd.html', {'event' : event_info, 'logged': False, 'wrongpwd': wrongpwd, 'alreadyregistered': alreadyregistered})
     ended = False
     if timezone.now() >= event_info.end_date:
         ended = True
@@ -57,25 +84,25 @@ def event(request, event_slug):
         begun = True
     challenges  = CTF.objects.filter(event=event_info).order_by('category', 'points')
     solved_list = Scores.objects.filter(event=event_info).order_by('-score', 'last_submission_date', 'user__username')
-    return render(request, 'events/event_info.html', {'event' : event_info, 'IsRegistered': IsRegistered, 'ctfs': challenges, 'solved_list':solved_list, 'ended': ended, 'begun': begun})
+    return render(request, 'events/event_info.html', {'event' : event_info, 'IsRegistered': IsRegistered, 'ctfs': challenges, 'solved_list':solved_list, 'ended': ended, 'begun': begun, 'wrongpwd': wrongpwd, 'alreadyregistered': alreadyregistered, 'subisover': subisover})
 
 @login_required
 def submit_event_flag(request, event_slug, chall_slug):
     ev          =   get_object_or_404(Event, slug=event_slug)
     response    =   redirect('events:event_chall_info', event_slug=event_slug, chall_slug=chall_slug)
     if timezone.now() >= ev.end_date:
-        response['Location'] += '?EventIsOver'
+        response['Location'] += '?EventIsOver=1'
         return response
 
     if request.method == 'POST':
         ctf_info    =   CTF.objects.get(event=ev, slug=chall_slug)
         if not ctf_info:
-            response['Location'] += '?ChallengeNotFound'
+            response['Location'] += '?ChallengeNotFound=1'
             return response
         flagged     =   False
         if CTF_flags.objects.filter(user=request.user, ctf=ctf_info):
             flagged = True
-            response['Location'] += '?AlreadyFlagged'
+            response['Location'] += '?AlreadyFlagged=1'
             return response
         try:
             score = Scores.objects.get(user=request.user, event=ev)
@@ -90,14 +117,17 @@ def submit_event_flag(request, event_slug, chall_slug):
                     score.last_submission_date = timezone.now()
                     score.score += ctf_info.points
                     score.save()
-                    response['Location'] += '?Congrat'
+                    response['Location'] += '?Congrat=1'
                     return response
                 else:
-                    response['Location'] += '?WrongFlag'
+                    response['Location'] += '?WrongFlag=1'
                     return response
             else:
-                response['Location'] += '?ErrorInForm'
+                response['Location'] += '?ErrorInForm=1'
                 return response
+        else:
+            response['Location'] += '?NotRegistered=1'
+            return response
     return response
 
 @login_required
@@ -107,15 +137,15 @@ def submit_pwd(request, event_slug):
         if request.user.is_authenticated:
             ev    =   get_object_or_404(Event, slug=event_slug)
             if ev == False:
-                response['Location'] += '?NoEventFound'
+                response['Location'] += '?NoEventFound=1'
                 return response
 
             if request.POST.get('password') != ev.password:
-                response['Location'] += '?WrongPassword'
+                response['Location'] += '?WrongPassword=1'
                 return response
 
             if Scores.objects.filter(user=request.user, event=ev).exists():
-                response['Location'] += '?AlreadyRegistered'
+                response['Location'] += '?AlreadyRegistered=1'
                 return response
             else:
                 new = Scores(user=request.user, event=ev, score=0)
@@ -129,13 +159,13 @@ def subscribe_to_event(request, event_slug):
         if request.user.is_authenticated:
             ev    =   get_object_or_404(Event, slug=event_slug)
             if ev == False:
-                response['Location'] += '?NoEventFound'
+                response['Location'] += '?NoEventFound=1'
                 return response
             if timezone.now() >= ev.end_date:
-                response['Location'] += '?SubscriptionIsOver'
+                response['Location'] += '?SubscriptionIsOver=1'
                 return response
             if Scores.objects.filter(user=request.user, event=ev).exists():
-                response['Location'] += '?AlreadyRegistered'
+                response['Location'] += '?AlreadyRegistered=1'
                 return response
             else:
                 new = Scores(user=request.user, event=ev, score=0)
