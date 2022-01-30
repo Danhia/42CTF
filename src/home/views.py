b352a2341af838d6867a0ceca29c967af9d44807
Danhia <danhia@protonmail.com>
2022-01-30 17:59:02 +0100

    added weekly top 5 + latest flags to home page

--- src/home/views.py
+++ src/home/views.py
@@ -8,6 +8,9 @@ from django.urls import translate_url
 from django.utils.translation import (
     LANGUAGE_SESSION_KEY, check_for_language, get_language,
 )
+import datetime
+from collections import defaultdict
+import operator
 
 def get_content_by_lang(news):
     lang = get_language()
@@ -21,6 +24,19 @@ def get_content_by_lang(news):
     elif lang == "ru":
         ret = news.content_ru
     return ret
+
+def get_weekly_top():
+    week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+    weekly_flags = CTF_flags.objects.filter(flag_date__gt=week_ago)
+    scores = defaultdict(int)
+
+    for sol in weekly_flags:
+            scores[sol.user] += sol.ctf.points
+
+    users = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+    users = [(u[0].userprofileinfo, u[1]) for u in users]
+
+    return(users[:5])
 
 def home(request):
     lang_code = get_language()
@@ -36,7 +52,11 @@ def home(request):
     top10       =   UserProfileInfo.objects.select_related().order_by('-score', 'last_submission_date', 'user__username')[:10]
     nb_flags    =   CTF_flags.objects.count()
     nb_users    =   UserProfileInfo.objects.count()
-    return render(request, 'home/home.html', {'news' : news, 'ctfs' : latest_ctfs, 'top' : top10, 'flags' : nb_flags})
+    latest_flags = CTF_flags.objects.order_by('-flag_date')[:5]
+    top_weekly = get_weekly_top()
+
+    return render(request, 'home/home.html', {'news' : news, 'ctfs' : latest_ctfs, 'top' : top10, 'flags' : nb_flags, 
+    'latest_flags':latest_flags, 'top_weekly': top_weekly, 'nb_users': nb_users})
 
 def cgu(request):
     return render(request, 'cgu.html')
