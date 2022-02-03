2f6362360b26f382ef2451769840f7f9d780e049
Danhia <danhia@protonmail.com>
2022-02-03 20:27:55 +0100

    added dynamic scoring for permanent platform, let's hope nothing breaks

--- src/ctfs/views.py
+++ src/ctfs/views.py
@@ -5,6 +5,8 @@ from .models import Category, CTF, CTF_flags
 from .forms import submit_flag
 from accounts.models import UserProfileInfo
 from django.utils.translation import get_language
+from math import log
+from accounts.models import UserProfileInfo
 
 def get_description_by_lang(ctf):
     lang = get_language()
@@ -18,6 +20,23 @@ def get_description_by_lang(ctf):
     elif lang == "ru":
         ret = ctf.description_ru
     return ret
+
+def actualize_points(ctf):
+    if ctf.category.name == "-Intro-":
+        return
+    solves =  CTF_flags.objects.filter(ctf=ctf)
+    nb_solves = len(solves)
+
+    new_points = 200 - int(log(nb_solves)*8.5)*5
+
+    if new_points != ctf.points:
+        diff = ctf.points - new_points
+        ctf.points = new_points
+        ctf.save()
+        for s in solves:
+            s.user.userprofileinfo.score -= diff
+            s.user.userprofileinfo.save()
+        
 
 def category(request, cat_slug):
     cat         =   get_object_or_404(Category, slug=cat_slug)
@@ -46,6 +65,7 @@ def ctf(request, cat_slug, ctf_slug):
                     profil.last_submission_date = timezone.now()
                     profil.score += ctf_info.points
                     profil.save()
+                    actualize_points(ctf_info)
                     return render(request, 'ctfs/ctf_info.html', { 'ctf' : ctf_info, 'solved_list': solved_list, 'valitated': True, 'description': description, 'date': timezone.now()})
                 else:
                     return render(request, 'ctfs/ctf_info.html', { 'ctf' : ctf_info, 'solved_list': solved_list, 'failed': True, 'description': description, 'date': timezone.now()})
